6c7068f6ed67ebb5a6742192527a5ea837601651
Swarit <swaritsharma2010@gmail.com>
2025-08-08 18:40:38 +0530

    Added hello world

--- Miscellaneous Projects/Pro_1.java
+++ Miscellaneous Projects/Pro_1.java
@@ -3,6 +3,6 @@ public class Pro_1 {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-            
+        System.out.println("Hello World !");
     }
 }

59c9895f067efe9823b7e3737ba1fcd5ad0385ad
Swarit <swaritsharma2010@gmail.com>
2026-01-27 20:28:51 +0530

    Started for loop named series

---
+++ Looping Projects/For Loop Projects/Series(Named)/Pro_01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pro_01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the terms :");
+        int n = sc.nextInt();
+        int t1 = 0;
+        int t2 = 1;
+        int sum;
+        for(int m = 1;m <= n - 2;m++){
+            sum = t1 + t2;
+            System.out.println(sum);
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}

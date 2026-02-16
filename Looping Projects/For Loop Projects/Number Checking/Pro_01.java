3f7d5f8e7ae57a76a48763de21b917a2a7e8d473
Swarit <swaritsharma2010@gmail.com>
2026-02-16 20:22:24 +0530

    Added more empty files

---
+++ Looping Projects/For Loop Projects/Number Checking/Pro_01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Pro_01 {
+
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("Enter the number :");
+            int n = sc.nextInt();
+            
+        }
+    }
+}
